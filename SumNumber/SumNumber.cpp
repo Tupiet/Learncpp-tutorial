9ff5575e6bf5ba898b26f5a7c3dafc87931b609c
Tupiet <Tupiet@users.noreply.github.com>
2021-01-30 16:48:53 +0100

    Added a new project - SumNumber!
    This commit adds a new project, called SumNumber. It only does that, it sums twi values that we input.

---
+++ SumNumber/SumNumber.cpp
@@ -0,0 +1,28 @@
+
+// This app will, simply, sum two numbers that we'll input from the console. It's an easy program. 
+
+#include <iostream> // For having std::cout and std::cin
+#include <limits>   // We need them for allowing the app to only close if the user press any key. 
+
+int main()
+{
+    std::cout << "Bienvenido a SumNumber!\n" << "Somos la aplicacion perfecta si no sabes sumar.\n";
+
+    std::cout << "Por favor, introduzca el primer numero.\n";
+    int x{ 0 };
+    std::cin >> x;
+
+    std::cout << "Ahora introduzca el segundo numero.\n";
+    int y{ 0 };
+    std::cin >> y;
+
+    std::cout << "La suma de " << x << " y " << y << " es igual a " << x + y;
+
+    // This will make the app stop, and wait before we press any button for closing it.
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+
+    return 0;
+}
+
